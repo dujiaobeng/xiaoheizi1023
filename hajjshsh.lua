917c53cbe9bec3face22da7220c8c0dbc2bc4007
xiaohei1022 <134212382+dujiaobeng@users.noreply.github.com>
2023-06-20 20:39:16 +0800

    Update hajjshsh.lua

--- hajjshsh.lua
+++ hajjshsh.lua
@@ -17,22 +17,21 @@ local credits = creds:section("UI设置",true)
             game:GetService("CoreGui")["frosty"].Main.Style = "Custom"
         end
     end)
-
+    credits:Button("关闭脚本",function()
+        game:GetService("CoreGui")["frosty"]:Destroy()
+    end)
 local creds = window:Tab("脚本")
 local credits = creds:section("付费",true)
     credits:Button("白脚本",function()
-        game:GetService("CoreGui")["frosty"]:Destroy()
 _G["白脚本作者修狗"]="xdjhadgdsrfcyefjhsadcctyseyr6432478rudghfvszhxcaheey"
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/wev666666/baijiaobengV2.0beta/main/%E7%99%BD%E8%84%9A%E6%9C%ACbeta'),true))() 
     end)
     credits:Button("走马观花x",function()
-        game:GetService("CoreGui")["frosty"]:Destroy()
 getgenv().zhanghuihuihui="走马观花作者 紅";
 loadstring(game:HttpGet("https://github.com/jsGay8s/RBX_DEV/raw/main/"..game.PlaceId..".lua"))("走马观花X-2023-04-24更新!")
     end)
 
 local credits = creds:section("免费",true)
     credits:Button("飞行",function()
-        game:GetService("CoreGui")["frosty"]:Destroy()
 loadstring([[loadstring(game:HttpGet(('https://gist.githubusercontent.com/meozoneYT/bf037dff9f0a70017304ddd67fdcd370/raw/e14e74f425b060df523343cf30b787074eb3c5d2/arceus%2520x%2520fly%25202%2520obflucator'),true))()]])()
     end)
