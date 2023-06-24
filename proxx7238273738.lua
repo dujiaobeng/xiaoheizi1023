397de90d8c06a6705b0d448e7046e2db474910c5
xiaohei1022 <134212382+dujiaobeng@users.noreply.github.com>
2023-06-24 15:51:43 +0800

    Update proxx7238273738.lua

--- proxx7238273738.lua
+++ proxx7238273738.lua
@@ -223,7 +223,7 @@ getgenv()._7461pro = "红脚本作者 坤" --> 不要改, 改了会被踢
 	
 		local develop = {  --> 脚本开发者列表, 里面双引号填写游戏用户名, 脚本开发者可以免去白名单检查
 			"4v5n7n",
-			"MIXI_SERVICE1",
+			"pro_2023625",
 			"cnmlgbnmsldsbcnm",
 			"baoanke",
 			"ydbfj138",
